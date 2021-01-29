8e849a177b84377c72fad0e82073efac63621c98
Sabine Pölzlbauer <sabine.poelzlbauer@gmail.com>
2021-01-29 20:05:56 +0100

    monaco setup for get worker url

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,28 @@ import "./App.css";
 import Editor from "@monaco-editor/react";
 import { editor, KeyCode, KeyMod } from "monaco-editor";
 
+const setupMonaco = () => {
+  (window as any).MonacoEnvironment = {
+    getWorkerUrl(moduleId: string, label: string) {
+      console.log(
+        "getWorkerUrl called from Module '" +
+          moduleId +
+          "' and with label '" +
+          label +
+          "'"
+      );
+      // if (label === "yaml") {
+      //   return "/vs/yaml.worker.js";
+      // }
+      return process.env.PUBLIC_URL + "/vs/editor.worker.js";
+    },
+  };
+};
+
 export const App = () => {
   const [value, setValue] = useState("yaml: myyaml");
   const [myEditor, setMyEditor] = useState<editor.IStandaloneCodeEditor>();
+  const [initial, setInitial] = useState(true);
 
   const saveDescriptor: editor.IActionDescriptor = {
     id: "saveElement",
@@ -16,6 +35,12 @@ export const App = () => {
       console.log("yuhu");
     },
   };
+
+  useEffect(() => {
+    if (initial === false) return;
+    setupMonaco();
+    setInitial(false);
+  }, [initial]);
 
   useEffect(() => {
     if (myEditor === undefined) return;
